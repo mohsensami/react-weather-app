2fe78906b49f56fa9398cdeee28fe28039192e65
mohsensami <mohsensami88@gmail.com>
2023-07-30 10:15:37 +0430

    show city information

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,46 @@
 import React, { useState } from "react";
 import "./App.css";
+import Layout from "./components/Layout";
+import timeConvert from "./utils/unixTimeConvert";
 
 function App() {
-  const [city, setCity] = useState<string>("");
+  const [input, setInput] = useState<string>("");
+  const [result, setResult] = useState<any>({});
+
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const handleSubmit = (e: React.MouseEvent<HTMLFormElement>) => {
     e.preventDefault();
+    setIsLoading(true);
+    fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?q=${input}&APPID=6557810176c36fac5f0db536711a6c52`
+    )
+      .then((res) => res.json())
+      .then((data) => setResult(data));
+    setIsLoading(false);
   };
   return (
-    <>
+    <Layout>
       <form onSubmit={handleSubmit}>
         <input
-          value={city}
-          onChange={(e) => setCity(e.target.value)}
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
           type="text"
         />
-        <input disabled={!city} type="submit" value="Send" />
+        <input disabled={!input} type="submit" value="Send" />
       </form>
-    </>
+      {isLoading ? (
+        <p>Loading ...</p>
+      ) : (
+        Object.keys(result).length !== 0 && (
+          <div>
+            <p>name: {result.city.name}</p>
+            <p>population: {result.city.population}</p>
+            <p>sunrise: {timeConvert(result.city.sunrise)}</p>
+            <p>sunrise: {timeConvert(result.city.sunset)}</p>
+          </div>
+        )
+      )}
+    </Layout>
   );
 }
 
