4e1057078f4cb046a4e698402f4bc0bac8ce4741
mohsensami <mohsensami18727@gmail.com>
2024-08-27 22:54:58 +0330

    Input added to Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import Inputs from './components/Inputs';
 import TopButtons from './components/TopButtons';
 
 function App() {
     return (
         <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br  h-fit shadow-xl shadow-gray-400">
             <TopButtons />
+            <Inputs
+            //  setQuery={setQuery} units={units} setUnits={setUnits}
+            />
         </div>
     );
 }
