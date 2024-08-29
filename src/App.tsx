2f546dd42210a168b74a966ed4b1c8cdd11d2684
mohsensami <mohsensami18727@gmail.com>
2024-08-29 23:38:04 +0330

    TemperatureAndDetails component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import Inputs from './components/Inputs';
+import TemperatureAndDetails from './components/TemperatureAndDetails';
 import TimeAndLocation from './components/TimeAndLocation';
 import TopButtons from './components/TopButtons';
 
 function App() {
     return (
-        <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br  h-fit shadow-xl shadow-gray-400">
+        <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-blue-400 bg-gradient-to-br  h-fit shadow-xl shadow-gray-400">
             <TopButtons />
             <Inputs
             //  setQuery={setQuery} units={units} setUnits={setUnits}
             />
             <TimeAndLocation
             // weather={weather}
-             />
+            />
+            <TemperatureAndDetails
+            //  weather={weather}
+            />
         </div>
     );
 }
