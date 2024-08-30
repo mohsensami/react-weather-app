44d08121d85c072ee7382db3bf557be71b82313b
mohsensami <mohsensami18727@gmail.com>
2024-08-30 21:42:10 +0330

    Forcast component added

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import Inputs from './components/Inputs';
 import TemperatureAndDetails from './components/TemperatureAndDetails';
 import TimeAndLocation from './components/TimeAndLocation';
 import TopButtons from './components/TopButtons';
-
 function App() {
     return (
         <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-blue-400 bg-gradient-to-br  h-fit shadow-xl shadow-gray-400">
@@ -16,6 +15,7 @@ function App() {
             <TemperatureAndDetails
             //  weather={weather}
             />
+            <FontFace />
         </div>
     );
 }
